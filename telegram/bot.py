d8e54c46488c850ce23a5fad3ebbbb930905353f
Vitor Canineo Komar <vitorcanineokomar@hotmail.com.br>
2023-08-24 15:41:23 -0300

    changed the files for the telegram bot to support users registering and patient registering

--- telegram/bot.py
+++ telegram/bot.py
@@ -2,54 +2,45 @@ import telepot
 from telepot.loop import MessageLoop
 import cherrypy
 import json 
+import time
+
+
+
+def registerNewPatient(chatId, patientId):
+    data = json.load(open('bot_settings.json'))
+    data[chatId]['patients'].append(patientId)
+    with open("bot_settings.json", "w") as file:
+        json.dump(data, file, indent = 4)
 
 class TelegramBot():
 
     def __init__(self, token):
         self.tokenBot = token
         self.bot = telepot.Bot(self.tokenBot)
-        self.chat_id = None
         self.conf = json.load(open('bot_settings.json'))
-        self.new = self.conf['new']
-        if self.new == False:
-            self.chat_id = self.conf['chat_id']
         MessageLoop(self.bot, {'chat': self.on_chat_message}).run_as_thread()
 
 
     def on_chat_message(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        if self.new:
-            self.chat_id = chat_id
-            self.conf['chat_id'] = chat_id
-            self.conf['new'] = False
-            json.dump(self.conf, open('bot_settings.json', 'w'))
-        self.bot.sendMessage(self.chat_id, text="Hello, thanks for registering")
-    
+        content_type, chat_type, chatId = telepot.glance(msg)
+        chatId = str(chatId)
+        text = msg['text']
+        
+        if chatId not in self.conf:
+            self.conf.update({chatId: {'patients': []}})
+            with open("bot_settings.json", "w") as file:
+                json.dump(self.conf, file, indent = 4)
+            self.bot.sendMessage(chatId, text="Hello, thanks for registering \n Use the following commands to interact with the bot:\n register patient_id \n check patient")
+        
+        else:
+            if 'register' in text:
+                patientId = text.split()[1]
+                registerNewPatient(chatId, patientId)
+                self.bot.sendMessage(chatId, text="New patient registered sucessfully")
             
-    def fever_alert(self):
-        print('function called')
-        self.bot.sendMessage(self.chat_id, "Fever Alert: Please check on the patient!")
     
-    def fall_alert(self):
-        self.bot.sendMessage(self.chat_id, "Fall Alert: Please check on the patient!")
-    
-    
-
-
-class Server:
-    def __init__(self, bot):
-        self.bot = bot
-
-    @cherrypy.expose
-    def fever(self):
-        self.bot.fever_alert()
-
-    @cherrypy.expose
-    def fall(self):
-        self.bot.fever_alert()
-
-
 
 if __name__ == '__main__':
     bot = TelegramBot("5837844672:AAGPkiYHwtHVQQ71ErsnVvA2u7PBIahxw_E")
-    cherrypy.quickstart(Server(bot))
+    while True:
+        time.sleep(3)
