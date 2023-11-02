4c3ad5eacf36ea835a06e669ecb1ada4ec68c0f6
Vitor Canineo Komar <vitorcanineokomar@hotmail.com.br>
2023-11-02 17:13:43 +0100

    added the fucntionality to receive alerts for the bot

--- telegram/bot.py
+++ telegram/bot.py
@@ -16,7 +16,7 @@ class TelegramBot():
     def __init__(self, token, catalogURL):
         self.catalogURL = catalogURL
         self.bot = telepot.Bot(token)
-        self.conf = json.load(open("telegram\settingsUpdated.json"))
+        self.conf = json.load(open("./settingsUpdated.json"))
         self.commands = ['/help: show information and brief instructions about available commands.', 
                          '/connect: connect to a monitoring device, user must provide device ID and password. /connect <DeviceID> <Password>',
                          '/associate: associate a device to the patient it is monitoring, user must provide device ID and patient name. /associate <DeviceID> <Name>',
@@ -52,15 +52,15 @@ class TelegramBot():
             pass
         else:
             self.conf[chatId]['patients'][deviceID] = ''
-            with open("telegram\settingsUpdated.json", "w") as file:
+            with open("./settingsUpdated.json", "w") as file:
                 json.dump(self.conf, file, indent = 4)
-            self.conf = json.load(open("telegram\settingsUpdated.json")) #update bot configs
+            self.conf = json.load(open("./settingsUpdated.json")) #update bot configs
 
     def associateDevice(self, chatId, deviceID, patientName):
         self.conf[chatId]['patients'][deviceID] = patientName
-        with open("telegram\settingsUpdated.json", "w") as file:
+        with open("./settingsUpdated.json", "w") as file:
             json.dump(self.conf, file, indent = 4)
-        self.conf = json.load(open("telegram\settingsUpdated.json")) #update bot configs
+        self.conf = json.load(open("./settingsUpdated.json")) #update bot configs
 
     def on_chat_message(self, msg):
         content_type, chat_type, chatId = telepot.glance(msg)
@@ -69,7 +69,7 @@ class TelegramBot():
         command = text.split()[0]
         if chatId not in self.conf:
             self.conf.update({chatId: {'patients': {}}})
-            with open("telegram\settingsUpdated.json", "w") as file:
+            with open("./settingsUpdated.json", "w") as file:
                 json.dump(self.conf, file, indent = 4)
             self.bot.sendMessage(chatId, text="Hello, thanks for contacting!\nType /help to see available commands.")
         else:
@@ -117,27 +117,22 @@ class TelegramBot():
                     self.bot.sendMessage(chatId, text='Patient is not being monitored. Please type /monitoring to see patients beeing monitored.')
                 else: 
                     self.conf[chatId]['patients'].pop(deviceID)
-                    with open("telegram\settingsUpdated.json", "w") as file:
+                    with open("./settingsUpdated.json", "w") as file:
                         json.dump(self.conf, file, indent = 4)
                     self.bot.sendMessage(chatId, text="Patient "+str(patientName)+' is no longer being monitored.')
                     self.bot.sendMessage(chatId, text="Device "+str(deviceID)+" is no longer connected.")
             else:
                 self.bot.sendMessage(chatId, text="Inavlid command, type /help to see available commands.")
 
+    
     """We need to finish data analysis in order to properly implement the following methods and check functionality"""
-    def send_fever_message(self, patient):
+    def send_fever_message(self, device):
         '''method that sends a message to every chat user 
         that are subscribed to a certatin patient'''
         for chatId in self.conf:
-            if patient in self.conf[chatId]['patients']:
+            if device in self.conf[chatId]['patients']:
                 self.bot.sendMessage(chatId, "Fever Alert: Please check on the patient!")
 
-    def send_fall_message(self, patient):
-        '''method that sends a message to every chat user 
-        that are subscribed to a certatin patient'''
-        for chatId in self.conf:
-            if patient in self.conf[chatId]['patients']:
-                self.bot.sendMessage(chatId, "Fall Alert: Please check on the patient!")
 
 class Server():
 
@@ -145,19 +140,28 @@ class Server():
         self.bot = bot
 
     @cherrypy.expose
-    def fever_alert(self, *uri):
-        patient = str(uri[0])
-        self.bot.send_fever_message(patient)
+    def alert(self, *uri):
+        '''this method will be used to receive every method, it will also get the metric and call the
+        right method of the bot'''
+        device = str(uri[0])
+        metric = str(uri[1])
+        print(uri)
+        #for now every alert will be fever so, just to test it
+        #the specific method for each alert will have to be specified 
+        self.bot.send_fever_message(device)
         
-    @cherrypy.expose
-    def fall_alert(self, *uri):
-        patient = str(uri[0])
-        self.bot.send_fever_message(patient)
-
+        '''
+        if metric == 'temperature':
+            self.bot.send_fever_alert(device)
+        elif metric == 'glucose':
+            self.bot.send_glucose_alert(device)'''
+        
+    
 if __name__ == '__main__':
 
     token = "6577470521:AAFTej1Dn-sOG6jE6xrYhvr9BHqudhI-SQg"
     catalogURL = "http://127.0.0.1:8083"
+    catalogURL = "http://192.168.11.43:8083"
 
     bot = TelegramBot(token, catalogURL)
     cherrypy.config.update({'server.socket_port': 1402})
