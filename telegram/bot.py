8736dff7a06acdd20b58cb0ecf7d28ead377b419
Vitor Canineo Komar <vitorcanineokomar@hotmail.com.br>
2023-11-26 09:51:48 +0100

    updated the bot to work with the correct format for the json file

--- telegram/bot.py
+++ telegram/bot.py
@@ -5,6 +5,7 @@ import json
 import time
 import random 
 import requests
+import os
 
 """Telegram bot that will allow interaction with wearable device 
     my opnion is that to understand the bot is better to run it and use it alongside with reading the code
@@ -16,7 +17,8 @@ class TelegramBot():
     def __init__(self, token, catalogURL):
         self.catalogURL = catalogURL
         self.bot = telepot.Bot(token)
-        self.conf = json.load(open("./settingsUpdated.json"))
+        self.path = os.path.join(os.path.curdir, 'bot_settings.json')
+        self.conf = json.load(open(self.path)) #I have put the updated file format on this file - vitor
         self.commands = ['/help: show information and brief instructions about available commands.', 
                          '/connect: connect to a monitoring device, user must provide device ID and password. /connect <DeviceID> <Password>',
                          '/associate: associate a device to the patient it is monitoring, user must provide device ID and patient name. /associate <DeviceID> <Name>',
@@ -26,17 +28,27 @@ class TelegramBot():
         ## need to better define check
         MessageLoop(self.bot, {'chat': self.on_chat_message}).run_as_thread()
 
+
+    def checkNewChatID(self, chatId):
+        '''checks if the chat ID has already connected with the bot, should return true if it is the first time'''
+        for el in self.conf:
+            if el['chatID'] == chatId:
+                return False
+        return True
+
     def hasID(self, deviceID):
-        for chatID in self.conf.keys():
-            for key in self.conf[chatID]['patients'].keys():
-                if key == deviceID:
+        for el in self.conf:
+            for device in el['devices']:
+                if device['deviceID'] == deviceID:
                     return True
         return False
 
     def isConnected(self, chatID, deviceID):
-        for key in self.conf[chatID]['patients'].keys():
-            if key == deviceID:
-                return True
+        for el in self.conf:
+            if el['chatID'] == chatID:
+                for device in el['devices']:
+                    if device['deviceID'] == deviceID:
+                        return True
         return False
     
     def verifyPassword(self, chatId, deviceID, givenPassword):
@@ -50,43 +62,73 @@ class TelegramBot():
     def registerDevice(self, chatId, deviceID):
         if self.hasID(deviceID):
             pass
-        else:
-            self.conf[chatId]['patients'][deviceID] = ''
-            with open("./settingsUpdated.json", "w") as file:
-                json.dump(self.conf, file, indent = 4)
-            self.conf = json.load(open("./settingsUpdated.json")) #update bot configs
+        else:            
+            for el in self.conf: 
+                if el['chatID'] == chatId:
+                    el['devices'].append({'deviceID': deviceID})
+                    with open(self.path, "w") as file:
+                        json.dump(self.conf, file, indent = 4)
+
 
     def associateDevice(self, chatId, deviceID, patientName):
-        self.conf[chatId]['patients'][deviceID] = patientName
-        with open("./settingsUpdated.json", "w") as file:
-            json.dump(self.conf, file, indent = 4)
-        self.conf = json.load(open("./settingsUpdated.json")) #update bot configs
+        for el in self.conf:
+            if el['chatID'] == chatId:
+                for device in el['devices']:
+                    if device['deviceID']==deviceID:
+                        device['name'] = patientName
+                        with open(self.path, 'w') as file:
+                            json.dump(self.conf, file, indent=4)
+
+                
+    def isMonitored(self, chatId, name):
+        '''returns whether or not a certain chat is monitoring a device'''
+        for el in self.conf:
+            if el['chatID'] == chatId:
+                for device in el['devices']:
+                    if device['name'] == name:
+                        return True
+
+        return False
+
+    
+    def checkPatient(self, metric, patientName):
+        '''method that send a get request to the data analysis rest server to get data'''
+        #still not implemented
+        pass
+
 
     def on_chat_message(self, msg):
         content_type, chat_type, chatId = telepot.glance(msg)
         chatId = str(chatId)
         text = msg['text']
         command = text.split()[0]
-        if chatId not in self.conf:
-            self.conf.update({chatId: {'patients': {}}})
-            with open("./settingsUpdated.json", "w") as file:
+
+        if self.checkNewChatID(chatId):
+            self.conf.append({"chatID": chatId, "devices": []})
+            with open(self.path, "w") as file:
                 json.dump(self.conf, file, indent = 4)
             self.bot.sendMessage(chatId, text="Hello, thanks for contacting!\nType /help to see available commands.")
+        
+        
         else:
             if command == '/help': 
                 self.bot.sendMessage(chatId, text='Available commands:')
                 for command in self.commands:
                     self.bot.sendMessage(chatId, text=command)
+
             elif command == '/start':
                 self.bot.sendMessage(chatId, text="Hello, thanks for contacting!\nType /help to see available commands.")
+
             elif command == '/connect':
                 deviceID = text.split()[1]
                 password = text.split()[2]
-                if self.hasID(deviceID) and self.verifyPassword(chatId, deviceID, password):
+                #if self.hasID(deviceID) and self.verifyPassword(chatId, deviceID, password):
+                if  self.verifyPassword(chatId, deviceID, password):
                     self.registerDevice(chatId, deviceID)
                     self.bot.sendMessage(chatId, text="Succesfully connected to device "+str(deviceID)+".")
                 else:
                     self.bot.sendMessage(chatId, text="Incorret credentials.")
+
             elif command == '/associate': 
                 deviceID = text.split()[1]
                 patientName = ' '.join(text.split()[2:]) 
@@ -95,32 +137,49 @@ class TelegramBot():
                     self.bot.sendMessage(chatId, text="Device "+str(deviceID)+' is monitoring patient '+str(patientName)+'.')
                 else:
                     self.bot.sendMessage(chatId, text="Device "+str(deviceID)+' is not connected.')
+
             elif command == '/monitoring':
-                if len(self.conf[chatId]['patients']) > 0:
+                for el in self.conf:
+                    if el['chatID'] == chatId:
+                        devicesList = el['devices']
+                
+
+                if len(devicesList) > 0:
                     patientsList = '```\n'
                     patientsList += 'ID : Name\n'
-                    for key, value in self.conf[chatId]['patients'].items():
-                        patientID = key
-                        patientName = value 
+
+                    for device in devicesList:
+                        patientID = device['deviceID']
+                        patientName = device['name'] 
                         patientsList += str(patientID)+' : '+str(patientName)+'\n'
                     patientsList += '```'
                     self.bot.sendMessage(chatId, text=patientsList, parse_mode='MarkdownV2')
+
                 else: 
                     self.bot.sendMessage(chatId, text='No patient is beeing monitored at the moment.')
+
             elif command == '/check':
                 #need to better understand this method
                 pass
+
             elif command == '/stopMonitoring':
                 deviceID = text.split()[1]
                 patientName = ' '.join(text.split()[2:])
-                if self.getName(chatId, patientID) is None:
+                if self.isMonitored(chatId, patientName) == False:
                     self.bot.sendMessage(chatId, text='Patient is not being monitored. Please type /monitoring to see patients beeing monitored.')
-                else: 
-                    self.conf[chatId]['patients'].pop(deviceID)
-                    with open("./settingsUpdated.json", "w") as file:
+                else:
+                    for el in self.conf:
+                        if el['chatID'] == chatId:
+                            devices = el['devices']
+                            for device in devices:
+                                if device['name'] == patientName:
+                                        devices.remove(device)
+                    
+                    with open(self.path, "w") as file:
                         json.dump(self.conf, file, indent = 4)
                     self.bot.sendMessage(chatId, text="Patient "+str(patientName)+' is no longer being monitored.')
                     self.bot.sendMessage(chatId, text="Device "+str(deviceID)+" is no longer connected.")
+
             else:
                 self.bot.sendMessage(chatId, text="Inavlid command, type /help to see available commands.")
 
@@ -161,8 +220,8 @@ if __name__ == '__main__':
 
     token = "6577470521:AAFTej1Dn-sOG6jE6xrYhvr9BHqudhI-SQg"
     catalogURL = "http://127.0.0.1:8083"
-    catalogURL = "http://192.168.11.43:8083"
-
+    #catalogURL = "http://192.168.11.43:8083"
+    
     bot = TelegramBot(token, catalogURL)
     cherrypy.config.update({'server.socket_port': 1402})
     cherrypy.quickstart(Server(bot))
