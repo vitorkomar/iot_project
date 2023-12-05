727952f3b8a52051a8d044af9aa309c03e15d13f
Vitor Canineo Komar <vitorcanineokomar@hotmail.com.br>
2023-12-05 09:33:05 +0100

    created the dataHandler directory to be a separate service from the analysis and added files there also did a simple implementation of the pressure alert

---
+++ dataHandler/storer.py
@@ -0,0 +1,15 @@
+from DataStorer import *
+import json
+import requests
+
+"""Basically run an instance of the data storer 
+    there shall be one instance for each patient
+    my idea is that it will be running on the wearable as soon as it turns on"""
+
+catalogURL = "http://127.0.0.1:8084"
+#catalogURL = "http://192.168.11.238:8083"
+
+storer = DataStorer(catalogURL, 10)
+storer.updateSettings()
+storer.run()
+
